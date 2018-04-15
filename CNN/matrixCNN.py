44c5d4e2e3d92da689c42126e6bf8dee0db07051
charelF <charel.felten@gmail.com>
2018-04-16 00:01:03 +0200

    CNN now working!

--- CNN/matrixCNN.py
+++ CNN/matrixCNN.py
@@ -9,6 +9,7 @@ from keras.layers import Conv2D #used for convolutional step, and 2D as images a
 from keras.layers import MaxPooling2D #Used for pooling step
 from keras.layers import Flatten #used to transform pooled maps into CNN input
 from keras.layers import Dense #used to add fully connected in a classic ANN
+from keras.layers import Reshape
 from keras.layers import Dropout
 from keras.optimizers import Adam #used as seen in Course 58 link of user apostolos
 from keras.preprocessing.image import ImageDataGenerator #used for image preprocessing
@@ -20,14 +21,17 @@ inputData = dsg.loadDataset("test_input.txt")
 outputData = dsg.loadDataset("test_output.txt")
 print("the size of the dataset is: ", inputData.shape, " of type: ", type(inputData))
 
+inputDataShape = inputData.shape
+# inputDataShape = tuple(amount of matrices, columns, rows)
+inputData = inputData.reshape(*inputDataShape, 1)
+outputData = outputData.reshape(inputDataShape[0], 1)
 
-# parameters
 
 # CNN
 
 cnn = Sequential()
 
-cnn.add(Conv2D(32, (3, 3), input_shape = inputData.shape, activation = 'relu'))
+cnn.add(Conv2D(32, (3, 3), input_shape = (inputDataShape[1], inputDataShape[2], 1), activation = 'relu'))
 
 cnn.add(MaxPooling2D(pool_size = (2, 2)))
 
@@ -43,7 +47,7 @@ cnn.summary()
 
 cnn.fit(inputData,
         outputData,
-        epochs=100,
+        epochs=20,
         validation_split=0.2)
 
 keepOpen=input()
