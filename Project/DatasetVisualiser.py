fb5741278530ae6662a3d549c38df67aaea751db
charelF <charel.felten@gmail.com>
2018-06-01 18:58:16 +0200

    added project readme

--- Project/DatasetVisualiser.py
+++ Project/DatasetVisualiser.py
@@ -3,19 +3,28 @@ import DatasetGenerator as dsg
 import glob
 import numpy as np
 
+# this code should not be reviewd as it has been created in a very limited
+# amount of time due do time contraints at the end of the BSP. It is included
+# in the project to given an easy way of visualising newly created datasets
+
+# to execute it, it is only necessary that there is a datasetgenerator.loadadataset
+# recognizable dataset combination in the project folder, then just execute the script
+
+# datasets need to have at least 8 entries and at least of size 2 by 2
+
 for ds in glob.glob("*_input.txt"):
 
     dataset = dsg.loadDataset(ds)
 
+    # creating a red colored delimitter matrix, used to display ds boundaries
     delimitterR = np.ones(dataset.shape[1:])
     delimitterG = np.zeros(dataset.shape[1:])
     delimitterB = np.zeros(dataset.shape[1:])
     delimitterRGB = np.stack((delimitterR, delimitterG, delimitterB), axis=2)
 
-    de = delimitterRGB[1:3]
-    deV = de.reshape(de.shape[1], de.shape[0], de.shape[2])
-
-    datasetPreview = np.vstack(tuple(dataset[i] for i in range(10)))
+    deHor = delimitterRGB[1:3]  # HORizontal DElinmitter
+    deVer = deHor.reshape(deHor.shape[1], deHor.shape[0], deHor.shape[2])
+    # VERtical DElinmitter
 
     entry0 = np.stack((dataset[0], dataset[0], dataset[0]), axis=2)
     entry1 = np.stack((dataset[1], dataset[1], dataset[1]), axis=2)
@@ -26,17 +35,17 @@ for ds in glob.glob("*_input.txt"):
     entry6 = np.stack((dataset[6], dataset[6], dataset[6]), axis=2)
     entry7 = np.stack((dataset[7], dataset[7], dataset[7]), axis=2)
 
-    vDelimitterMissingPart = np.stack((np.ones((de.shape[0]*3,de.shape[0])),
-                                       np.zeros((de.shape[0]*3,de.shape[0])),
-                                       np.zeros((de.shape[0]*3,de.shape[0]))),
-                                      axis=2)
+    deVerMissingPart = np.stack((np.ones((deHor.shape[0]*3,deHor.shape[0])),
+                                          np.zeros((deHor.shape[0]*3,deHor.shape[0])),
+                                          np.zeros((deHor.shape[0]*3,deHor.shape[0]))),
+                                         axis=2)
 
-    vDelimitter = np.vstack((deV, deV, deV, deV, vDelimitterMissingPart))
+    deVerCombined = np.vstack((deVer, deVer, deVer, deVer, deVerMissingPart))
 
-    datasetPreviewR = np.vstack((entry1, de, entry3, de, entry5, de, entry7))
+    datasetPreviewR = np.vstack((entry1, deHor, entry3, deHor, entry5, deHor, entry7))
 
-    datasetPreviewL = np.vstack((entry0, de, entry2, de, entry4, de, entry6))
+    datasetPreviewL = np.vstack((entry0, deHor, entry2, deHor, entry4, deHor, entry6))
 
-    datasetPreviewCombined = np.hstack((datasetPreviewR, vDelimitter, datasetPreviewL))
+    datasetPreviewCombined = np.hstack((datasetPreviewR, deVerCombined, datasetPreviewL))
 
-    scipy.misc.imsave(ds+".png", datasetPreviewCombined)
+    scipy.misc.imsave(ds[:-4]+".png", datasetPreviewCombined)
---
+++ Project/README.md
@@ -0,0 +1,36 @@
+#This is the project folder of the BiCS-BSP-2
+
+##datasets folder
+This folder contains a few datasets and their visual representations and was created to not convolute the project directory with the datasets. Newly created datasets will still be in the project folder, and datasets need also to be in the project folder for the networks to be able to train on them. (The reason why it is not possible to use them in a subdirectory is because this would be difficult to implement when considering that all the code should work on Windows
+and UNIX OS's
+
+##CNN-simple, ANN-simple
+These are our two main neural networks, an artificial and a convolutional neural networks. To train them, it is necessary that corresponding input and output dataset text files are in the same folder as the neural networks
+
+##DatasetGenerator
+Contains the create and loadDataset functions
+
+##MatrixGenerator
+Contains the Matrix, EmptyMatrix and FeatureMatrix classes
+
+##DatasetCreationTool
+A simple script to create datasets very quickly by executing the python file. It uses the MatrixGenerator
+
+##DatasetVisualizer
+A simple script to create a visual representation of any dataset currently in the project folder
+
+##test_input.txt, test_output.txt, test_input.png
+These are sample datasets, which are not meant to be used to train an neural network.
+
+##ANN-HPO, CNN-HPO:
+
+these are the Hyperparameter optimised versions of the Neural Networks. They are only briefly mentioned in the report as we did not manage to complete them to the level
+we desired. The CNN-HPO for example only trys out different network strucutures (i.e. which types of layers and hown many of them). But due to the exponential nature of
+the code, we have 1024 different networks. (Using Gridsearch and setting the cross validation paramter to 3 (as is per default), this number would rise to over 3000
+runs) This means that if the CNN-simple network takes 1 time unit for a given dataset, the CNN-HPO dataset takes over 3000 time units. Altough this is still a feasible
+time, it get's worse when we consider that the CNN only optimises a low amount of values. 
+
+To demonstrate the complexity, we included much more parameters to be optimized in the ANN network. In its current state, the ANN would technically need 2^68 time units
+As this is not physically possible to do, we gave default values for all paramters in the function definition and then separated the parameters dictionary into several subparts
+which would need to be optimized individually and manually. Especially the fact that we were unable to come up with an automatic solution and would need to optimize each
+parameter set manually convinced us that the HPO networks would not be finished for the BSP and we thus decided to not present their code in the production section.
